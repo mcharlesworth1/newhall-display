9df5ce85aefbc6b7ba8017fe74d6598e32648a49
mcharlesworth1 <m.charlesworth@newhallschool.co.uk>
2025-09-05 16:34:26 +0100

    Update getCalendar.js

--- newhall-display/netlify/functions/getCalendar.js
+++ newhall-display/netlify/functions/getCalendar.js
@@ -1,26 +1,20 @@
-// This is the Node.js code for our serverless function
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-exports.handler = async function(event, context) {
-    // The calendar URL is safely stored here on the server-side
+export const handler = async (event, context) => {
     const CALENDAR_URL = 'https://www.newhallschoolcalendar.co.uk/CalendarSync.ashx?Cal=New%20Hall%20School%20Calendar&ID=3282';
 
     try {
         const response = await fetch(CALENDAR_URL);
         const data = await response.text();
-
         return {
             statusCode: 200,
-            headers: {
-                // This header is crucial - it allows your webpage to access the data
-                'Access-Control-Allow-Origin': '*', 
-            },
+            headers: { 'Access-Control-Allow-Origin': '*' },
             body: data,
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch calendar data.' }),
+            body: JSON.stringify({ error: error.message }),
         };
     }
 };
